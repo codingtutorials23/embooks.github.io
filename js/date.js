2cdc116da06f222d31ce2cadbf0c2b548c80d429
writebooksonline <89612530+EmBooks@users.noreply.github.com>
2021-10-25 09:29:57 -0400

    Create date.js

---
+++ js/date.js
@@ -0,0 +1,34 @@
+const time = new Date().getHours()
+let greeting;
+if(time < 10){
+  greeting = "Good morning";
+}else if (time < 20){
+  greeting = "Good day";
+}else{
+  greeting = "Good evening";
+}
+let day;
+switch(new Date().getDay()){
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day ="Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+    
+}
+document.write("Today Is:" + day "and the time is:" + time);
